b44611fe302293a5f30176e87c342e99005c0312
KorenkovVA <VKorenkov@qulix.com>
2019-12-05 18:57:35 +0300

    Web interface

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Switch, Route, Link, BrowserRouter as Router} from "react-router-dom";
 import './App.css';
+import Main from './containers/Main'
+import Dashboard from "./containers/Dashboard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+
+      <Router>
+        <Link to="/">Dashboard</Link>
+        <Switch>
+          <Route path="/">
+            <Main/>
+          </Route>
+          <Route path="/dashboard">
+          </Route>
+        </Switch>
+      </Router>
+    </React.Fragment>
   );
 }
 
